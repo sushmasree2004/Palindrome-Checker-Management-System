8a273db38c30f3269303a3da5194b1111d1896bc
sushmasree2004 <sushmasree2004@gmail.com>
2026-03-03 15:56:22 +0530

    commit for Application Entry & Welcome Message

---
+++ UseCaseOnePalindromeCheckerApp.java
@@ -0,0 +1,25 @@
+package palindrome;
+/**
+ * Use Case 1 :Application Entry and  Welcome Message 
+ * 
+ * 
+ * Description :
+ * This class represents the entry point of the 
+ * Palindrome  Checker Management System
+ * 
+ * 
+ * Author  : Sushma Sree
+ * Version : 1
+ */
+
+public class UseCaseOnePalindromeCheckerApp 
+{
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to Palindrome Checker Management System");
+		System.out.println("Version : 1.0 ");
+		System.out.println("System intialized Sucessfully !!");
+	}
+	
+
+}
